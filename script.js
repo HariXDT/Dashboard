994a886c767116a5fa24298fddd8e0b51f5f28ca
HariXDT <harishankarsahu50@gmail.com>
2024-11-09 01:13:22 +0530

    update for DTH 11

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyD4r9IZ4nDeFWVTv4W52nB_13l1YC6d7fc",
-    authDomain: "toggle-button-3d8fe.firebaseapp.com",
-    databaseURL: "https://toggle-button-3d8fe-default-rtdb.firebaseio.com",
-    projectId: "toggle-button-3d8fe",
-    storageBucket: "toggle-button-3d8fe.appspot.com",
-    messagingSenderId: "807063547069",
-    appId: "1:807063547069:web:0ca44bccf198b408de1bd4",
-    measurementId: "G-7Q81S5145B"
+    apiKey: "AIzaSyBNP-XmUIxdnRdQ0wRR9tuwmcmN8-QZK0o",
+    authDomain: "smart-energy-meter-4835a.firebaseapp.com",
+    databaseURL: "smart-energy-meter-4835a-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "smart-energy-meter-4835a",
+    storageBucket: "smart-energy-meter-4835a.appspot.com",
+    messagingSenderId: "67753854056",
+    appId: "1:67753854056:web:77598c3b44ce544022e7dd",
+    measurementId: "G-1GXEBJ5LER"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
@@ -14,10 +14,11 @@ var firebaseConfig = {
 $(document).ready(function(){
     var database = firebase.database();
 	var Led1Status;
+    var Led2Status;
 
 	database.ref().on("value", function(snap){
-		Led1Status = snap.val().Led1Status;
-		if(Led1Status == "1"){    // check from the firebase
+		Led2Status = snap.val().Led2Status;
+		if(Led2Status == "1"){    // check from the firebase
 			//$(".Light1Status").text("The light is off");
 			document.getElementById("unact").style.display = "none";
 			document.getElementById("act").style.display = "block";
@@ -29,14 +30,32 @@ $(document).ready(function(){
 	});
 
     $(".toggle-btn").click(function(){
-		var firebaseRef = firebase.database().ref().child("Led1Status");
+		var firebaseRef = firebase.database().ref().child("Led2Status");
 
-		if(Led1Status == "1"){    // post to firebase
+		if(Led2Status == "1"){    // post to firebase
 			firebaseRef.set("0");
-			Led1Status = "0";
+			Led2Status = "0";
 		} else {
 			firebaseRef.set("1");
-			Led1Status = "1";
+			Led2Status = "1";
 		}
 	})
 });
+
+// getting reference to the database
+var database = firebase.database();
+
+//getting reference to the data we want
+var dataRef1 = database.ref('DHT_11/Humidity');
+var dataRef2 = database.ref('DHT_11/Temperature');
+
+//fetch the data
+dataRef1.on('value', function(getdata1){
+	var humi = getdata1.val();
+	document.getElementById('humidity').innerHTML = humi + "%";
+})
+
+ dataRef2.on('value', function(getdata2){
+	var temp = getdata2.val();
+	document.getElementById('temperature').innerHTML = temp + "&#8451;";
+})
